e24c62a42a67ceca0d55731c821c9451f2b4c2c5
TreyRandall12 <jamesnrandall12@gmail.com>
2021-03-01 18:41:07 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -34,7 +34,7 @@ var init = function (window) {
         
         // TODO 3 / 7 : Call the drawCircle() function 
 
-        for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
              drawCircle();
         };
         ////////////////////////////////////////////////////////////
